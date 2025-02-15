e67bb8682bf9c5558f2e842d17f6a3838f29bfb1
Vaibhav Mali <malivaibhav.7620@gmail.com>
2025-02-15 20:14:37 +0530

    used FileReader api to new fileReader instance & final changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,22 @@ function App() {
   function handleImageUpload(e) {
     const file = e.target.files[0];
     console.log(file);
+
+    if(file){
+      const reader = new FileReader();
+     reader.onload = () =>{
+      setImage(reader.result)
+     }
+
+      reader.readAsDataURL(file);
+    }
   }
 
   return (
-    <>
+    <div className="container">
       <input type="file" accept="image/*" onChange={handleImageUpload} />
-    </>
+      {image && <img src={image} alt="uploaded" width={400}/>}
+    </div>
   );
 }
 
